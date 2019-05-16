cc7ba419c8dd2c522da347eb6e42b08d1186af23
Charles Harold Llanto <47489593+SlapmewithCommend@users.noreply.github.com>
2019-05-15 23:01:30 -0700

    Major Update

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
-const logo = document.querySelectorAll("#logo path");
-for (let i = 0; i<logo.length; i++) {
-    console.log(`letter ${i} is ${logo[i].getTotalLength()}`);
-}
+// const logo = document.querySelectorAll("#logo path");
+// for (let i = 0; i<logo.length; i++) {
+//     console.log(`letter ${i} is ${logo[i].getTotalLength()}`);
+// }
+
+
+
+// window.onscroll = function() {scrollFunction()};
+
+// function scrollFunction() {
+//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//     document.getElementById("navbar").style.top = "0";
+//   } else {
+//     document.getElementById("navbar").style.top = "-50px";
+//   }
+// }
